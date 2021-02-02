7019dee7b9dc238c121b2bff666acd00861e5eb7
BuiQuangDuy99 <duybeo19121999@gmail.com>
2021-02-02 16:54:09 +0700

    commit cho Tung magre

--- MISA.QLTSv2/wwwroot/js/Base/common.js
+++ MISA.QLTSv2/wwwroot/js/Base/common.js
@@ -1,1 +1,11 @@
-﻿
+﻿function callAjax(url, method, data) {
+    $.ajax({
+        url: url,
+        method: method,
+        data: JSON.stringify(data)
+    }).done(function (res) {
+        return res;
+    }).fail(function (res) {
+        return res;
+    })
+}
