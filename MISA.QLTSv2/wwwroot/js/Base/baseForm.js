14bfdfd26e80c18a28509ebb812739c37b76d9a3
ndtung <ndtung@FRESHER-56>
2021-02-02 16:37:35 +0700

    Gui BQDUY

---
+++ MISA.QLTSv2/wwwroot/js/Base/baseForm.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+
+    //Định nghĩa Dialog
+    dialog = $(".dialog__content").dialog({
+        autoOpen: true,
+        height: 751,
+        width: 750,
+        modal: true,
+
+
+    });
+})
