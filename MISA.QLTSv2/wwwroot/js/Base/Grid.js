7429b28207f2bc7ca861d506e26d31db4bddab40
ndtung <ndtung@FRESHER-56>
2021-02-03 17:47:31 +0700

    baseForm

--- MISA.QLTSv2/wwwroot/js/Base/Grid.js
+++ MISA.QLTSv2/wwwroot/js/Base/Grid.js
@@ -1,1 +1,16 @@
-﻿
+﻿$(document).ready(function () {
+
+    //Định nghĩa Dialog
+    dialog = $(".dialog__content").dialog({
+        autoOpen: true,
+        height: 751,
+        width: 750,
+        modal: true,
+    });
+    new grid("#dialog");
+})
+class grid extends baseForm {
+    constructor(formId) {
+        super(formId);
+    }
+}
--- MISA.QLTSv2/wwwroot/js/Base/baseForm.js
+++ MISA.QLTSv2/wwwroot/js/Base/baseForm.js
@@ -1,12 +1,88 @@
-﻿$(document).ready(function () {
+﻿
+//-----------------Form-----------------------------
+class baseForm {
+    ///constructor
+    constructor(Idform) {
 
-    //Định nghĩa Dialog
-    dialog = $(".dialog__content").dialog({
-        autoOpen: true,
-        height: 751,
-        width: 750,
-        modal: true,
+        this.form = $(Idform);
+        this.initEvent();
+    };
+    initEvent() {
+
+        this.form.find("#btn-cancel").off("click");
+        this.form.find("#btn-save").off("click");
+
+        this.form.find("input[required]").on("blur", this.checkInputRequired);
+        this.form.find("input[required]").on("keyup", this.checkInputRequired);
+        this.form.find("#btn-cancel").on("click",this.closeForm);
+    }
+
+    /**
+     * Validate bắt buộc nhập 
+     * CreatedBy : NDTUNG (3/2/2021)
+     */
+    checkInputRequired() {
+        var val = $(this).val();
+
+        if (val.trim() == "") {
+            $(this).addClass('border-red');
+            $(this).attr('title', 'Trường này không được để trống');
+            $(this).attr('Validate', false);
+        }
+        else {
+            $(this).removeClass('border-red');
+            $(this).removeAttr('title');
+            $(this).attr('Validate', true);
+        }
+
+    }
 
 
-    });
-})
+    /**
+         * Reset lại form
+         * CreatedBy : NDTUNG (3/2/2021)
+         */
+    resetForm() {
+        this.form.find("[fieldName]").each(function () {
+            $(this).val("");
+        });
+        this.form.find(".border-red").removeClass(".border-red");
+    }
+
+    /**
+     * Đóng form
+     * CreatedBy : NDTUNG (3/2/2021)
+     */
+    closeForm() {
+        this.resetForm;
+        dialog.dialog('close');
+    }
+
+    /**
+     * Lấy dữ liệu trong form
+     * CreatedBy : NDTUNG (3/2/2021)
+     */
+    getData() {
+        var data = {};
+        var inputs = this.form.find("[fieldName]");
+        $.each(inputs, function (index, input) {
+            var propertyName = $(this).attr("fieldName");
+            var value = $(this).val();
+            data[propertyName] = value;
+        });
+        return data;
+    }
+
+
+     /**
+     * Đổ dữ liệu vào trong form
+     * CreatedBy : NDTUNG (3/2/2021)
+     */
+    bindingData(data) {
+        this.form.find("[fieldName]").each(function () {
+            var propertyName = this.attr('fieldName');
+            var propertyValue = data[propertyName];
+            this.value = propertyValue;
+        });
+    }
+}
