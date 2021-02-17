6863177c405748b7e3fbd36bd83b5bf62a463d42
ndtung <ndtung@FRESHER-56>
2021-02-17 10:17:58 +0700

    form

---
+++ MISA.QLTSv2/wwwroot/js/Form/dictionaryForm.js
@@ -0,0 +1,17 @@
+﻿$(document).ready(function () {
+
+    //Định nghĩa Dialog
+    dialog = $(".dialog__content").dialog({
+        autoOpen: true,
+        height: 751,
+        width: 750,
+        modal: true,
+    });
+    new dictionaryForm("#dialog_dictionary");
+
+})
+class dictionaryForm extends baseForm {
+    constructor(formId) {
+        super(formId);
+    }
+}
