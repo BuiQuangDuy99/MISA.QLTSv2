5bdcbe0e7ac159647e19eccd342b37daca19060f
ndtung <ndtung@FRESHER-56>
2021-02-17 10:54:12 +0700

    Form tạm

--- MISA.QLTSv2/wwwroot/js/Form/dictionaryForm.js
+++ MISA.QLTSv2/wwwroot/js/Form/dictionaryForm.js
@@ -1,13 +1,19 @@
 ﻿$(document).ready(function () {
 
     //Định nghĩa Dialog
-    dialog = $(".dialog__content").dialog({
-        autoOpen: true,
-        height: 751,
-        width: 750,
+    dialog = $(".dialog_dictionary").dialog({
+        autoOpen: false,
+        height: 600,
+        width: 800,
         modal: true,
     });
+
     new dictionaryForm("#dialog_dictionary");
+
+    $(".btn-add").click(function() {
+        dialog.dialog('open');
+
+    })
 
 })
 class dictionaryForm extends baseForm {
