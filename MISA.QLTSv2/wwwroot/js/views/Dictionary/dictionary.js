902acbb085cfd1ad1b332523603fb025fc354c3f
BuiQuangDuy99 <duybeo19121999@gmail.com>
2021-02-26 11:57:24 +0700

    commit

--- MISA.QLTSv2/wwwroot/js/views/Dictionary/dictionary.js
+++ MISA.QLTSv2/wwwroot/js/views/Dictionary/dictionary.js
@@ -2,7 +2,7 @@
     $('#cbx-test').combobox();
     var day = new Date("2020-10-01");
     console.log(day);
-    var date = formatDate("29-02-2020", 'DD/MM/YYYY');
+    var date = formatDate("04-29-2020", 'DD/MM/YYYY');
     console.log(date);
 })
 
@@ -21,11 +21,16 @@ class Dictionary extends BaseGrid {
         showTooltipElement($('td'));
     }
 
+    /**
+     * Hàm khởi tạo
+     * @param {any} formID
+     * @param {any} width
+     * @param {any} height
+     */
     createFormDetail(formID, width, height) {
         var me = this;
         this.formDetail = new dictionaryForm(formID, width, height, me);
         this.formDetail.initLoadComboBox("AssetGroup", assetGroup);
-        //this.formDetail.initLoadComboBox("AssetClass", dictionary);
     }
 }
 
