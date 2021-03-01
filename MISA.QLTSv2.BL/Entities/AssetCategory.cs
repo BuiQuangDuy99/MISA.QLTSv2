ef7322c4b5edaf80ee428ae4b04fe8ff77ff21d7
tuyen nguyen <nguyentuyen4921@gmail.com>
2021-03-01 13:41:36 +0700

    commit

---
+++ MISA.QLTSv2.BL/Entities/AssetCategory.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.QLTSv2.BL.Entities
+{
+    public class AssetCategory:BaseEntity
+    {
+        #region Declare
+        #endregion
+        #region Constructor
+        public AssetCategory()
+        {
+
+        }
+        #endregion
+        #region properties
+        /// <summary>
+        /// Khóa chính
+        /// </summary>
+        public Guid AssetCategoryId { get; set; }
+        /// <summary>
+        /// Mã loại tài sản
+        /// </summary>
+        public string AssetCategoryCode { get; set; }
+        /// <summary>
+        /// Có phải là cha không
+        /// </summary>
+        public Boolean? IsParent { get; set; }
+        /// <summary>
+        /// ID loại tài sản cha
+        /// </summary>
+        public Guid ParentId { get; set; }
+        /// <summary>
+        /// Loại tài sản này thuộc loại tài sản lớn hơn nào
+        /// </summary>
+        public string ParentName { get; set; }
+        /// <summary>
+        /// Tên loại tài sản
+        /// </summary>
+        public string AssetCategoryName { get; set; }
+        /// <summary>
+        /// Id của đơn vị
+        /// </summary>
+        public Guid? OrganizationId { get; set; }
+        /// <summary>
+        /// Tỷ lệ hao mòn (%)
+        /// </summary>
+        public float? DepreciationRate { get; set; }
+        /// <summary>
+        /// Số năm sử dụng 
+        /// </summary>
+        public int? LifeTime { get; set; }
+        /// <summary>
+        /// Ghi chú
+        /// </summary>
+        public string Description { get; set; }
+        #endregion
+        #region Method
+        #endregion
+    }
+}
---
+++ MISA.QLTSv2.BL/Models/fixed_asset_category.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.QLTSv2.BL.Models
+{
+    /// <summary>
+    /// Loại tài sản
+    /// </summary>
+    public class fixed_asset_category
+    {
+        #region Declare
+        #endregion
+        #region Constructor
+        public fixed_asset_category()
+        {
+
+        }
+        #endregion
+        #region properties
+        /// <summary>
+        /// Khóa chính
+        /// </summary>
+        public Guid fixed_asset_category_id { get; set; }
+        /// <summary>
+        /// Mã loại tài sản
+        /// </summary>
+        public string fixed_asset_category_code { get; set; }
+        /// <summary>
+        /// Có phải là cha không
+        /// </summary>
+        public Boolean? is_parent { get; set; }
+        /// <summary>
+        /// ID loại tài sản cha
+        /// </summary>
+        public Guid parent_id { get; set; }
+        /// <summary>
+        /// Loại tài sản này thuộc loại tài sản lớn hơn nào
+        /// </summary>
+        public string parent_name { get; set; }
+        /// <summary>
+        /// Tên loại tài sản
+        /// </summary>
+        public string fixed_asset_category_name { get; set; }
+        /// <summary>
+        /// Id của đơn vị
+        /// </summary>
+        public Guid? organization_id { get; set; }
+        /// <summary>
+        /// Tỷ lệ hao mòn (%)
+        /// </summary>
+        public float? depreciation_rate { get; set; }
+        /// <summary>
+        /// Số năm sử dụng 
+        /// </summary>
+        public int? life_time { get; set; }
+        /// <summary>
+        /// Ghi chú
+        /// </summary>
+        public string description { get; set; }
+        /// <summary>
+        /// Người Lập
+        /// </summary>
+        public string created_by { get; set; }
+        /// <summary>
+        /// Ngày lập
+        /// </summary>
+        public DateTime? created_date { get; set; }
+        /// <summary>
+        /// Người sửa
+        /// </summary>
+        public string modified_by { get; set; }
+        /// <summary>
+        /// Ngày sửa
+        /// </summary>
+        public DateTime? modified_date { get; set; }
+        #endregion
+        #region Method
+        #endregion
+    }
+}
