a02260bb989c68b2d521dc0325387438bda78d6f
BuiQuangDuy99 <duybeo19121999@gmail.com>
2021-03-01 14:07:06 +0700

    commit

--- MISA.QLTSv2.DL/BaseRepository.cs
+++ MISA.QLTSv2.DL/BaseRepository.cs
@@ -1,11 +1,36 @@
-﻿using MISA.QLTSv2.BL.Interfaces;
+﻿using MISA.QLTSv2.BL.Entities;
+using MISA.QLTSv2.BL.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MISA.QLTSv2.DL
 {
-    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : BaseEntity
     {
+        public int Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(Guid entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetEntityById(Guid entityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- MISA.QLTSv2/Startup.cs
+++ MISA.QLTSv2/Startup.cs
@@ -1,13 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using MISA.QLTSv2.BL.Interfaces;
+using MISA.QLTSv2.DL;
 
 namespace MISA.QLTSv2
 {
@@ -25,6 +22,8 @@ namespace MISA.QLTSv2
         {
             services.AddControllersWithViews();
             services.AddCors();
+
+            services.AddScoped(typeof(IBaseRepository<>), typeof(BaseRepository<>));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
