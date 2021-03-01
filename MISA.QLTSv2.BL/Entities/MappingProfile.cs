06093aa07ce5898e074bc745c54051efa09c097e
tuyen nguyen <nguyentuyen4921@gmail.com>
2021-03-01 13:50:47 +0700

    commit

---
+++ MISA.QLTSv2.BL/Entities/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MISA.QLTSv2.BL.Models;
+using MISA.QLTSv2.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.QLTSv2.BL.Entities
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<fixed_asset, Asset>();
+            CreateMap<fixed_asset_category, AssetCategory>();
+            CreateMap<department, Department>();
+        }
+    }
+}
--- MISA.QLTSv2/Startup.cs
+++ MISA.QLTSv2/Startup.cs
@@ -1,9 +1,11 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MISA.QLTSv2.BL.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +25,15 @@ namespace MISA.QLTSv2
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            var mapperConfig = new MapperConfiguration(mc =>
+            {
+                mc.AddProfile(new MappingProfile());
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            services.AddMvc();
             services.AddControllersWithViews();
             services.AddCors();
         }
