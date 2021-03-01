488048ffe617ee0ff0845efb57ea72ea77ccff94
BuiQuangDuy99 <duybeo19121999@gmail.com>
2021-03-01 13:42:38 +0700

    commit

---
+++ MISA.QLTSv2.DL/BaseRepository.cs
@@ -0,0 +1,11 @@
+﻿using MISA.QLTSv2.BL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MISA.QLTSv2.DL
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity>
+    {
+    }
+}
