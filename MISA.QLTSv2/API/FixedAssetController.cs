9b86d5d6ece1fa70690b7cb479b4d3763abd684f
tuyen nguyen <nguyentuyen4921@gmail.com>
2021-03-01 16:47:25 +0700

    commit

---
+++ MISA.QLTSv2/API/FixedAssetController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MISA.QLTSv2.BL.Entities;
+using MISA.QLTSv2.BL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MISA.QLTSv2.API
+{
+
+    public class FixedAssetController : BaseEntityController<FixedAsset>
+    {
+        IFixedAssetService _fixedAssetService;
+        public FixedAssetController(IFixedAssetService fixedAssetService) : base(fixedAssetService)
+        {
+            _fixedAssetService = fixedAssetService;
+        }
+    }
+}
