3e04c156cbb7d8f24ab49e9fac59a0ae28a9fb8c
ndtung <ndtung@FRESHER-666>
2021-03-02 13:30:54 +0700

    Hao mon

--- MISA.QLTSv2/wwwroot/js/Form/depreciationForm.js
+++ MISA.QLTSv2/wwwroot/js/Form/depreciationForm.js
@@ -1,4 +1,5 @@
-﻿class depreciationForm extends baseForm {
+﻿
+class depreciationForm extends baseForm {
     constructor(formId, width, height, jsCaller) {
         super(formId, jsCaller);
         //Định nghĩa Dialog
@@ -8,6 +9,78 @@
             width: width,
             modal: true,
         });
+        this.initEventDepreciationForm();
+        showTooltipElement($('.depreciation-sub-grid th'));
+
+
+    }
+
+    /**
+     * Hàm tạo sự kiện trên form Depreciation
+     * */
+    initEventDepreciationForm() {
+        var me = this;
+        super.initEvent();
+
+        $('.btn-add-row').off('click').click(function () {
+            me.addRow();
+        });
+
+        $('.depreciation-sub-grid').on('click', 'tr td button', (function () {
+            me.deleteRow($(this));
+        }));
+
+        $('.btn-delete-all-row').off('click').click(function () {
+            me.deleteAllRow();
+        });
+
+    }
+
+    /**
+     * Hàm tạo một dòng khi nhấn click "Thêm dòng"
+     */
+    addRow() {
+        let me = this,
+            tr = $(`<tr>
+                        <td class="text-alight-center"></td>
+                        <td></td>
+                        <td></td>
+                        <td class="text-alight-right"></td>
+                        <td class="text-alight-center"></td>
+                        <td class="text-alight-right"></td>
+                        <td><button class="button btn-depr-delete hide" title="Xóa"><div class="icon-delete-row"></div></button></td>
+                    </tr>`);
+
+        $('#depreciation-sub-grid tbody').append(tr);
+        me.bindingSTT();
+
+    }
+
+    /**
+     * Hàm binding tự động STT
+     * Create
+     * */
+    bindingSTT() {
+        $('.depreciation-sub-grid tbody tr').each(function (index) {
+            {
+                $(this).find("td").eq(0).text(index + 1);
+            }
+        });
+    }
+
+    /**
+     * Hàm xóa một dòng khi nhấn click nút xóa trên dòng
+     */
+    deleteRow(button) {
+        $(button).parents('tr').remove();
+    }
+
+    /**
+     * Hàm xóa toàn bộ dòng
+     * CreatedBy:NDTUNG (2/3/2021)
+     * */
+    deleteAllRow() {
+        $('.depreciation-sub-grid tbody').empty();
     }
 
     show(data) {
