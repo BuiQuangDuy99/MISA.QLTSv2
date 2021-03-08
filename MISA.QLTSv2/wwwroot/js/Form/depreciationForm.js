1886fe7bd76e6bd8966f5e14c91ba11b137945b7
Tung Dang <tungbeng.31.1.1999@gmail.com>
2021-03-08 10:17:40 +0700

    Push(8/3)

--- MISA.QLTSv2/wwwroot/js/Form/depreciationForm.js
+++ MISA.QLTSv2/wwwroot/js/Form/depreciationForm.js
@@ -4,7 +4,7 @@ class depreciationForm extends baseForm {
         super(formId, jsCaller);
         //Định nghĩa Dialog
         this.depreciationForm = $(formId).dialog({
-            autoOpen: false,
+            autoOpen: true,
             height: height,
             width: width,
             modal: true,
@@ -34,10 +34,6 @@ class depreciationForm extends baseForm {
             me.deleteAllRow();
         });
         me.formatTd();
-
-        $('input[fieldName="RefNo"]').off('keyup').keyup(function () {
-            this.value = this.value.toLocaleUpperCase();
-        });
     }
 
     /**
@@ -49,9 +45,9 @@ class depreciationForm extends baseForm {
                         <td class="text-alight-center"></td>
                         <td><input type="text" class="input-depreciation-sub-grid"></td>
                         <td></td>
-                        <td class="text-alight-right"></td>
-                        <td class="text-alight-center"></td>
-                        <td class="text-alight-right"></td>
+                        <td class="text-alight-right" dataType="Money" ></td>
+                        <td class="text-alight-right dataType="Number" "></td>
+                        <td class="text-alight-right" dataType="Money"></td>
                         <td><button class="button btn-depr-delete hide" title="Xóa"><div class="icon-delete-row"></div></button></td>
                     </tr>`);
 
@@ -125,23 +121,41 @@ class depreciationForm extends baseForm {
 
     getData() {
         let depreciation = {},
-            asset = {},
             listAsset = [];
-        $('.depreciation-sub-grid tbody tr').each(function (index) {
+        $('.depreciation-sub-grid tbody tr').each(function () {
+            let asset = {},
+                fieldNameAss,
+                dataType;
             $(this).find('td[fieldName]').each(function () {
-                let fieldNameAss = $(this).attr('fieldName');
-                asset[fieldNameAss] = $(this).prop("textContent");
+
+                fieldNameAss = $(this).attr('fieldName');
+                dataType = $(this).attr("dataType");
+
+                if (dataType == "Number" || dataType == "Money") {
+                    asset[fieldNameAss] = parseInt($(this).prop("textContent"));
+                }
+                else {
+                    asset[fieldNameAss] = $(this).prop("textContent");
+                }
+                if (fieldNameAss == "AmountTotal") {
+                    let AmountTotal = asset['Cost'] / 100 * asset['DepreciationRate'];
+                    asset[fieldNameAss] = AmountTotal;
+                    $(this).append(AmountTotal);
+                }
             })
             listAsset.push(asset);
         })
+
         $('input[fieldName],textarea[fieldName],table[fieldName]').each(function () {
             let fieldName = $(this).attr('fieldName');
             if (fieldName == "RefDetail") {
                 depreciation[fieldName] = listAsset;
             }
-            else
+            else {
                 depreciation[fieldName] = $(this).val();
+            }
         });
         console.log(depreciation);
     }
+
 }
