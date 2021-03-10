f7f518c4ef6a07986e136599686e9eac6b555cd4
dvvuong <dvvuong@FRESHER-666>
2021-03-10 17:24:11 +0700

    update refDecrement

---
+++ MISA.QLTSv2/wwwroot/js/views/Asset/refDecrement.js
@@ -0,0 +1,80 @@
+﻿$(document).ready(function () {
+
+})
+
+
+class refDecrement extends BaseGrid {
+
+    constructor(gridId, entity) {
+        super(gridId, entity);
+        this.initEvents();
+    }
+
+    initEvents() {
+        let me = this;
+        super.initEvents();
+        showTooltipElement($('button'));
+        showTooltipElement($('td'));
+    }
+
+    setUrl() {
+        this.url = 'https://localhost:44363/api/refdecrements';
+    }
+
+    createFormDetail(formID, width, height) {
+        var me = this;
+        this.formDetail = new reftransferForm(formID, width, height, me);
+    }
+}
+
+var refTransferGrid = new refTransfer('#refdecrement-grid', "RefDecrement");
+
+// Biến config cho từng column trong bảng
+
+var conFigColum = [
+    {
+        DataType: "STT",
+        FieldName: "STT",
+        FieldText: "STT",
+        Index: 1
+    },
+    {
+        DataType: "datetime",
+        FieldName: "PostedDate",
+        FieldText: "Ngày ghi giảm",
+        Index: 2
+    },
+    {
+        DataType: "text",
+        FieldName: "RefNo",
+        FieldText: "Số chứng từ",
+        Index: 3
+    },
+    {
+        DataType: "text",
+        FieldName: "JournalMemo",
+        FieldText: "Lý do ghi giảm",
+        Index: 4
+    },
+    {
+        DataType: "money",
+        FieldName: "RefDetail",
+        FieldText: "Giá trị còn lại",
+        Index: 5
+    },
+    {
+        DataType: "function",
+        FieldName: "Function",
+        FieldText: "Chức năng",
+        Index: 5
+    }
+];
+
+//khởi tạo form ghi tăng tài sản
+//refTransferGrid.createFormDetail("#dialog_reftransfer", 800, 600);
+
+// THiết lập config header
+refTransferGrid.setConFigColum(conFigColum);
+
+// Load dữ liệu grid
+refTransferGrid.loadAjaxData('https://localhost:44363/api/refdecrements');
