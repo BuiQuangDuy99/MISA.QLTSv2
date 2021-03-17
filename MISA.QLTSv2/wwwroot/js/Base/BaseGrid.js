8b958c8e66e84f9ca8bb0563051351559a0251e6
BuiQuangDuy99 <duybeo19121999@gmail.com>
2021-03-17 10:54:06 +0700

    commit

--- MISA.QLTSv2/wwwroot/js/Base/BaseGrid.js
+++ MISA.QLTSv2/wwwroot/js/Base/BaseGrid.js
@@ -55,10 +55,7 @@ class BaseGrid extends Grid {
             }
         });
 
-        $('.close, #btn-no-warring,#btn-ok-warring').off('click').click(closeWarring);
-        $('#btn-yes-warring').off('click').click(function () {
-            me.delete();
-        });
+      
         //$('#btn-delete').on("click", function () {
         //    me.confirmDelete();
         //    $('.close, #btn-no-warring,#btn-ok-warring').off('click').click(closeWarring);
@@ -108,18 +105,11 @@ class BaseGrid extends Grid {
         $('.loading').show();
 
         let selectedRow = me.getListId();
-        // Xử lý biến thành chuỗi để ném vào store ['','',''] , "'id1','id2','id3'"
-        let ids = "";
-        if (selectedRow) {
-            $.each(selectedRow, function (index, id) {
-                ids += "'" + String(id) + "',";
-            });
-            ids = ids.slice(0, -1);
-        }
         var url = me.url;
         $.ajax({
-            url: `${url}?ids=${ids}`,
+            url: url,
             method: "DELETE",
+            data: JSON.stringify(selectedRow),
             contentType: 'application/json',
             async: true
         }).done(function (res) {
