dcab4372bf2726a58c87dc97cadbed65f1f94b91
BuiQuangDuy99 <duybeo19121999@gmail.com>
2021-03-23 23:59:46 +0700

    commit duy

--- MISA.QLTSv2/wwwroot/js/Form/assetForm.js
+++ MISA.QLTSv2/wwwroot/js/Form/assetForm.js
@@ -8,14 +8,11 @@
             width: width,
             modal: true,
         });
+        this.autocomplete();
     }
 
     initEvent() {
         super.initEvent();
-        $('#cbxdepartment').change(function () {
-            $('#txtdepartment').val($(this).val());
-        })
-
     }
 
     setApiUrl() {
@@ -27,6 +24,71 @@
      * */
     changeValueSelection() {
        
+    }
+
+    autocomplete() {
+        var me = this;
+        // Gọi ajax để lấy danh sách phòng ban
+        $.ajax({
+            url: 'https://localhost:44363/api/Departments',
+            method: "GET"
+        }).done(function (data) {
+            var arr = [];
+            // Build lại danh sách phòng ban để thực hiện được autocomplete
+            $.each(data, function (index, object) {
+                object["label"] = object["DepartmentCode"];
+                object["value"] = object["DepartmentCode"];
+                arr.push(object);
+            });
+            // Thêm sự kiện focus ô input thì sẽ bắn menu, dùng sự kiện search của autocomplte jueryui
+            $('#txtDepartment').autocomplete({
+                delay: 0,
+                minLength: 0,
+                source: arr,
+                select: function (event, ui) {
+                    $('#txtDepartmentName').val(ui.item.DepartmentName);
+                }
+            }).on("focus", function () {
+                $(this).autocomplete("search", '');
+            }).autocomplete("instance")._renderItem = function (ul, item) {
+                return $("<li>")
+                    .attr("title", item.label + " - " + item.DepartmentName)
+                    .append($("<div>").text(item.label + " - " + item.DepartmentName))
+                    .appendTo(ul);
+            };
+        }).fail(function (data) {
+
+        });
+        $.ajax({
+            url: 'https://localhost:44363/api/v1/FACategories',
+            method: "GET"
+        }).done(function (data) {
+            var arr = [];
+            // Build lại danh sách phòng ban để thực hiện được autocomplete
+            $.each(data.Data, function (index, object) {
+                object["label"] = object["FixedAssetCategoryCode"];
+                object["value"] = object["FixedAssetCategoryCode"];
+                arr.push(object);
+            });
+            // Thêm sự kiện focus ô input thì sẽ bắn menu, dùng sự kiện search của autocomplte jueryui
+            $('#txtFixedAssetCategoryCode').autocomplete({
+                delay: 0,
+                minLength: 0,
+                source: arr,
+                select: function (event, ui) {
+                    $('#txtFixedAssetCategoryName').val(ui.item.FixedAssetCategoryName);
+                }
+            }).on("focus", function () {
+                $(this).autocomplete("search", '');
+            }).autocomplete("instance")._renderItem = function (ul, item) {
+                return $("<li>")
+                    .attr("title", item.label + " - " + item.FixedAssetCategoryName)
+                    .append($("<div>").text(item.label + " - " + item.FixedAssetCategoryName))
+                    .appendTo(ul);
+            };
+        }).fail(function (data) {
+
+        });
     }
 
     show(data) {
